5310dc4a2bcf02cfe263aa994e1a6dcc8d078ef8
ayushmaanghorui <143436450+ayushmaanghorui@users.noreply.github.com>
2023-09-19 16:57:02 +0530

    Add files via upload

---
+++ area of square.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int a,area;
+	
+	printf("Enter side of the square");
+	scanf("%d",&a);
+	
+	area=a*a;
+	printf("area is %d", area);
+	return 0;
+}
---
+++ cube.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int a,b;
+	printf("Enter any number :");
+	scanf("%d", &a);
+	
+	b=pow(a,3);
+	printf("%d is cube of %d", b,a);
+	return 0;
+	
+	
+}
