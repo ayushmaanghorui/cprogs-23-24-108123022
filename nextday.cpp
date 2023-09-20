51287b5452813fbe0fb54ee21554d086273882eb
ayushmaanghorui <143436450+ayushmaanghorui@users.noreply.github.com>
2023-09-20 16:51:46 +0530

    Add files via upload

--- nextday.cpp
+++ nextday.cpp
@@ -4,176 +4,213 @@ int main()
 	int d,m,y;
 	printf("Enter day, month and year : ");
 	scanf("%d%d%d", &d,&m,&y);
-
-	
-	
 	if(y%4==0&&(y%100!=0||y%400==0))
-	
-	
 	{
-		if(m>12&&m<1)
-		{
-			printf("Invalid");
-		}
-		if(d>31&&d<1)
-		{
-			printf("Invalid");
-		}
 		if(m==2)
 		{
-			if(d==29)
+			if(d>29)
 			{
-			
-				d=1;
-				m=m+1;
-				printf("%d %d %d", d,m,y);
+				printf("Invalid");
 			}
 			else
 			{
-				d=d+1;
-				
-				printf("%d %d %d", d,m,y);
-				
+				if(d==29)
+				{
+					d=1;
+					m=m+1;
+					printf("%d %d %d", d,m,y);
+				}
+				else
+				{
+					d=d+1;
+					printf("%d %d %d", d,m,y);
+				}
 			}
 		}
 		else
 		{
 			if(m==1||m==3||m==5||m==7||m==8||m==10)
 			{
-				if(d==31)
+				if(d>31)
 				{
-					d=1;
-					m=m+1;
-					printf("%d %d %d", d,m,y);
+					printf("Invalid");
 				}
 				else
 				{
-					d=d+1;
-					printf("%d %d %d", d,m,y);
-					
+					if(d==31)
+					{
+						d=1;
+						m=m+1;
+						printf("%d %d %d", d,m,y);
+					}
+					else
+					{
+						d=d+1;
+						printf("%d %d %d", d,m,y);
+						
+					}
 				}
 			}
 			else
 			{
-				if(d==31)
+				if(m==12)
 				{
-					printf("Invald");
-				}
-				if(d==30)
-				{
-					d=1;
-					m=m+1;
-					printf("%d %d %d", d,m,y);
-				}
-				else
-				{
-					d=d+1;
-					printf("%d %d %d", d,m,y);
-					
-				}
-				
-			}
-			if(m==12)
-			{
-				if(d==31)
-				{
-					d=1;
-					m=1;
-					y=y+1;
-					printf("%d %d %d", d,m,y);
+					if(d>31)
+					{
+						printf("Invalid");
+					}
+					else
+					{
+						if(d==31)
+						{
+							d=1;
+							m=1;
+							y=y+1;
+							printf("%d %d %d", d,m,y);
+						}
+						else
+						{
+							d=d+1;
+							printf("%d %d %d", d,m,y);
+						}
+					}
 				}
 				else
 				{
-					d=d+1;
-					printf("%d %d %d",d,m,y);
+					if(m==4||m==6||m==9||m==11)
+					{
+						if(d>30)
+						{
+							printf("Invalid");
+						}
+						else
+						{
+							if(d==30)
+							{
+								d=1;
+								m=m+1;
+								printf("%d %d %d", d,m,y);
+							}
+							else
+							{
+								d=d+1;
+								printf("%d %d %d", d,m,y);
+								
+							}
+						}
+					}
+					else
+					{
+						printf("invalid");
+					}
 				}
 			}
 		}
 	}
 	else
 	{
-		
-		if(m>12&&m<1)
-		{
-			printf("Invalid");
-		}
-		if(d>31&&d<1)
-		{
-			printf("Invalid");
-		}
+	
 		if(m==2)
 		{
-			if(d==29)
+			if(d>28)
 			{
 				printf("Invalid");
 			}
-			if(d==28)
-			{
-			
-				d=1;
-				m=m+1;
-				printf("%d %d %d", d,m,y);
-			}
 			else
 			{
-				d=d+1;
-				
-				printf("%d %d %d", d,m,y);
-				
+				if(d==28)
+				{
+					d=1;
+					m=m+1;
+					printf("%d %d %d", d,m,y);
+				}
+				else
+				{
+					d=d+1;
+					printf("%d %d %d", d,m,y);
+				}
 			}
 		}
 		else
 		{
 			if(m==1||m==3||m==5||m==7||m==8||m==10)
 			{
-				if(d==31)
+				if(d>31)
 				{
-					d=1;
-					m=m+1;
-					printf("%d %d %d", d,m,y);
+					printf("Invalid");
 				}
 				else
 				{
-					d=d+1;
-					printf("%d %d %d", d,m,y);
-					
+					if(d==31)
+					{
+						d=1;
+						m=m+1;
+						printf("%d %d %d", d,m,y);
+					}
+					else
+					{
+						d=d+1;
+						printf("%d %d %d", d,m,y);
+						
+					}
 				}
 			}
 			else
 			{
-				if(d==31)
+				if(m==12)
 				{
-					printf("Invalid");
-				}
-				if(d==30)
-				{
-					d=1;
-					m=m+1;
-					printf("%d %d %d", d,m,y);
-				}
-				else
-				{
-					d=d+1;
-					printf("%d %d %d", d,m,y);
-					
-				}
-				
-			}
-			if(m==12)
-			{
-				if(d==31)
-				{
-					d=1;
-					m=1;
-					y=y+1;
-					printf("%d %d %d", d,m,y);
+					if(d>31)
+					{
+						printf("Invalid");
+					}
+					else
+					{
+						if(d==31)
+						{
+							d=1;
+							m=1;
+							y=y+1;
+							printf("%d %d %d", d,m,y);
+						}
+						else
+						{
+							d=d+1;
+							printf("%d %d %d", d,m,y);
+						}
+					}
 				}
 				else
 				{
-					d=d+1;
-					printf("%d %d %d",d,m,y);
+					if(m==4||m==6||m==9||m==11)
+					{
+						if(d>30)
+						{
+							printf("Invalid");
+						}
+						else
+						{
+							if(d==30)
+							{
+								d=1;
+								m=m+1;
+								printf("%d %d %d", d,m,y);
+							}
+							else
+							{
+								d=d+1;
+								printf("%d %d %d", d,m,y);
+								
+							}
+						}
+					}
+					else
+					{
+						printf("invalid");
+					}
 				}
 			}
 		}
 	}
+	
+	
 }
